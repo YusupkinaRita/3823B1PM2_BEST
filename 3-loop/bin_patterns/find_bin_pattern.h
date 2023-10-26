4670caad4466ab956142d9815ca863c338075d17
YusupkinaRita <yusupkina.rita@gmail.com>
2023-10-26 17:56:50 +0300

    Update c-cpp.yml

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,5 +1,12 @@
 #include <math.h>
+#include <stdio.h>
 
 char find_bin_pattern(int number){
-    return 100;
+    int a=0;
+    while(number>0){
+        if ((number&7)==5)
+            a++;
+        number=number>>1;
+    }
+    return a;
 }
