4c295b0ba231f1ea3efdc135d3fca86fa8c9da28
YusupkinaRita <yusupkina.rita@gmail.com>
2023-11-05 00:41:41 +0300

    elementatemysanity

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -2,19 +2,17 @@
 
 int find_elem(int left, int right, int number, int max_iter){
     int i = 0;
-    int x=0, t=0, l=0;
+    int x=0, t=0, l=0, d=0;
     l=left;
     for(int i1=1;i1<=max_iter;i1++){
-        if(left==l)
-            x=(right-left)/2;
-        else
-            x=left+(right-left)/2;
-        if(x==number){
-            t=i1;break;}
-        if(number>x)
-            left=x;
-        if(number<x)
-            right=x;
+    x=(left + right) / 2;
+    if (x == number) {
+        t=i1;
+        break;}
+    if (x < number) 
+        left = x + 1;
+    else 
+        right =x - 1;
     }
     i=(number-l)*t;
 
