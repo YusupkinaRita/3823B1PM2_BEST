32d8c785b317ab66aa338c8718db750c85cd2308
YusupkinaRita <yusupkina.rita@gmail.com>
2023-11-04 17:54:04 +0300

    row

--- 3-loop/row/calc_row.h
+++ 3-loop/row/calc_row.h
@@ -1,3 +1,18 @@
+#include<math.h>
 long long calc_row(int n, long long k){
-    return 0;
+    long long s=0;
+    for(int i=1;i<=n;i++){
+        s=s+i;
+    }
+    long long k1=k;
+    long long suml=s*k;
+    if (suml<0)
+        suml=suml*(-1);
+    if(k<0)
+        k=k*(-1);
+    unsigned long long sum=s*k;
+    if(sum==suml)
+        return s*k1;
+    else
+        return 0;
 }
