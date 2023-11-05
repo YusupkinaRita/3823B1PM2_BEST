e7c19852056f92a6ade6db08c4bea817485b007c
YusupkinaRita <yusupkina.rita@gmail.com>
2023-11-05 21:51:21 +0300

    limit

--- 3-loop/limit/limit.h
+++ 3-loop/limit/limit.h
@@ -1,5 +1,15 @@
 #include <math.h>
 
 double limit(double eps, int n_max){
-    return 0.0;
+    double n=0, n0=0;
+    for (int i=1;i<=n_max;i++ ){
+        n=i/(sqrt(i*i+i));
+        if(i>1){
+            n0=(i-1)/sqrt((i-1)*(i-1)+(i-1));
+            if(n-n0<=eps)
+                break;
+        }
+        
+    }
+    return n;
 }
