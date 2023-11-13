1b09c5d9aa49ceec2bab0a404afa1faa8a9324a6
YusupkinaRita <yusupkina.rita@gmail.com>
2023-11-13 19:50:03 +0300

    djk

--- 4-array/little_big_converter/little_big_convert.h
+++ 4-array/little_big_converter/little_big_convert.h
@@ -1,5 +1,12 @@
 #include <math.h>
 
 unsigned int little_big_convert(unsigned int input){
-    return 0;
+    unsigned char x1, x2, x3, x4;
+    x1=input;
+    x2=input>>8;
+    x3=input>>16;
+    x4=input>>24;
+    unsigned int l_end= x1*pow(256,3)+x2*256*256+x3*256+x4;
+
+    return l_end;
 }
