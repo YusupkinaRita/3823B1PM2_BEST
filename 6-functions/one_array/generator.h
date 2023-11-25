b8d73f6cfbd4c2ffdf30da1528cd89ba6f7e0945
YusupkinaRita <yusupkina.rita@gmail.com>
2023-11-25 17:57:15 +0300

    sf

--- 6-functions/one_array/generator.h
+++ 6-functions/one_array/generator.h
@@ -1,5 +1,20 @@
 #include <math.h>
+#include <stdlib.h>
+
+int* arrgen(int size, int f, int k ){
+    int* arr=malloc(sizeof(int)*size);
+    arr[0]=0;
+    arr[1]=f;
+    for (int i=2; i<size; i++){
+        arr[i]=(k-f)/(size-2);
+    }
+    return arr;
+}
+
 
 int* array_generator(int* real_size, int k, int m, int f){
-    return 0;
+    *real_size=10+rand()%91;
+    int* arr=arrgen(*real_size, f,k);
+    
+    return arr;
 }
