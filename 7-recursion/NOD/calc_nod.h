2c3eb53dc8ba6852c84b59491e19392cc3379149
YusupkinaRita <yusupkina.rita@gmail.com>
2023-11-28 23:51:54 +0300

    nod

--- 7-recursion/NOD/calc_nod.h
+++ 7-recursion/NOD/calc_nod.h
@@ -1,5 +1,18 @@
 #include <math.h>
 
+int find_nod(int first, int second){
+
+    int r=second%first;
+    if (r==0)
+    return first;
+    if (find_nod(r,first)==0)
+        return r;
+    
+    return find_nod(r,first);
+
+}
 int nod(int first, int second){
+    if(first<1|| second<1)
     return -1;
+    return find_nod(first, second);
 }
