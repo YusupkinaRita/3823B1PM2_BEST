d8523dea53a5aa42927bab6d0e7e68891f1291d8
YusupkinaRita <yusupkina.rita@gmail.com>
2023-11-29 22:29:25 +0300

    sum

--- 7-recursion/recursion_array_sum/calc_sum_array_elements.h
+++ 7-recursion/recursion_array_sum/calc_sum_array_elements.h
@@ -1,5 +1,16 @@
 #include <math.h>
 
+double sum(int* array, int size){
+    while (size>=0){
+    if (size==0)
+    return array[0];
+    return array[size]+sum(array,size-1);
+    }
+}
+
 double calc_sum_elements(int* array, int array_size){
+    if(array_size<=0)
     return -1.0;
+    int x=sum(array,array_size-1);
+    return sum(array,array_size-1);
 }
