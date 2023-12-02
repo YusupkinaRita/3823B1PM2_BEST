6e8071af89df275cc094fb302297a79b1ba7bc24
YusupkinaRita <yusupkina.rita@gmail.com>
2023-12-02 18:30:57 +0300

    jkl;

--- 7-recursion/new_series/find_series_sum.h
+++ 7-recursion/new_series/find_series_sum.h
@@ -1,5 +1,26 @@
 #include <math.h>
+double num ( int k){
+    if (k==0)
+    return 0.0;
+    return (num(k-1)+1.0)/k;
+    
+}
+
+double sum(int k){
+    while(k>=0){
+    if (k==0)
+    return 0.0;
+    double x=num(k);
+    return x+sum(k-1);
+    }
+}
 
 double find_sum_elements_series(int k){
-    return -1.0;
+    if(k==0)
+    return 0;
+    if (k<0)
+    return -1;
+    else
+    return sum(k-1);
+ 
 }
--- 7-recursion/revers_array/revers.h
+++ 7-recursion/revers_array/revers.h
@@ -1,5 +1,17 @@
 #include <math.h>
 
+void revar(int* arr, int size, int i){
+    int x;
+    if(i>=size)
+        return ;
+    x=arr[i];
+    arr[i]=arr[size];
+    arr[size]=x;
+    revar(arr,size-1,i+1);
+}
+
 void reverse_array(int* array, int array_size){
 
+    if(array_size>1)
+    revar(array, array_size-1, 0);
 }
