40cba50a57853f076ef3fead205c908e9bcb392e
YusupkinaRita <yusupkina.rita@gmail.com>
2023-12-05 23:47:15 +0300

    ghjk

--- 8-strings/str_to_double/str_to_double_convert.h
+++ 8-strings/str_to_double/str_to_double_convert.h
@@ -2,5 +2,32 @@
 #include <string.h>
 
 double convert(char num[]){
-    return 0.0;
+    double x=0, n=0;
+    int id=0, s=0;
+    double d=0;
+    if(num[0]!=45){
+        for (int i=0;num[i]!=46;i++){
+            x=x*10 + (num[i]-48);
+            id=i+1;
+            }
+    }
+    else{
+        for (int i=1;num[i]!=46;i++){
+            x=x*10 + (num[i]-48);
+            id=i+1; 
+            }
+    }
+
+    for (int i=id+1;num[i]!='\0';i++){
+        d=d*10+(num[i]-48);
+        s++;
+    }
+    if(d!=0)
+    n=x+d/(pow(10,s));
+    else
+    n=x;
+    if(num[0]==45)
+    n=(-1)*n;
+
+    return n;
 }
