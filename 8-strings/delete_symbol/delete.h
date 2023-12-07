df5ad6c6a24453b67285b048e5283fcf6efe7b0e
YusupkinaRita <yusupkina.rita@gmail.com>
2023-12-07 20:31:29 +0300

    yay

--- 8-strings/delete_symbol/delete.h
+++ 8-strings/delete_symbol/delete.h
@@ -2,6 +2,22 @@
 #include <string.h>
 
 void DelSymbol1(char *str, char sym){
+    int ind=0;
+    int am=0;
+    for(int i=0;i<strlen(str);i++){
+        if (str[i]==sym)
+        am++;
+    }
+    while(am!=0){
+        for(int i=0;i<strlen(str);i++){
+            if(str[i]==sym)
+                ind=i;
+        }
+        for (int i = ind;i<strlen(str); i++) {
+            str[i] = str[i+1];
+        }
+        am--;
 
+    }
 }
 
--- 8-strings/to_lower/lower_converter.h
+++ 8-strings/to_lower/lower_converter.h
@@ -2,5 +2,9 @@
 #include <string.h>
 
 void lower(char* str){
+    for(int i=0;i<strlen(str);i++){
+        if(str[i]>=65&&str[i]<=90)
+            str[i]=str[i]+32;
+    }
 
 }
