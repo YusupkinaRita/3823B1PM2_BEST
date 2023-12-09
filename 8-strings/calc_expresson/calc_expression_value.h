95de332b7fa4bc249d05c460e17419f0f0ba7db9
YusupkinaRita <yusupkina.rita@gmail.com>
2023-12-09 16:36:29 +0300

    hgfgffgfgfgf

--- 8-strings/calc_expresson/calc_expression_value.h
+++ 8-strings/calc_expresson/calc_expression_value.h
@@ -2,5 +2,28 @@
 #include <string.h>
 
 int calculate_expression(char expression[]){
-    return 0;
+    int res=0;
+    int num=0;
+    char operation=0;
+    if(expression[0]=='-')
+        operation='-';
+    else
+    operation='+';
+
+    for (int i = 0; i<strlen(expression); i++) {
+        if (expression[i] >= '0' && expression[i] <= '9') {
+            num = num * 10 + (expression[i] - '0');
+        }
+        if (expression[i] == '+' || expression[i] == '-'|| i==strlen(expression)-1 ) {
+        if (operation == '+') 
+        res += num;
+        else 
+        res -= num;
+        num = 0;
+        operation = expression[i];
+        }
+        if (expression[i]=='-'&& expression[i+1]=='+'||expression[i]=='+'&& expression[i+1]=='-') 
+        return -1;
+    }
+    return res;
 }
