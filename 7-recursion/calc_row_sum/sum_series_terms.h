20c2b58b1cb2ab7d312ecf6feaf971edb11c10e6
YusupkinaRita <yusupkina.rita@gmail.com>
2023-12-26 20:37:20 +0300

    fghj

--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -1,5 +1,14 @@
 #include <math.h>
+double sum (int n){
+    if(n==1)
+        return 1.0;
+    return 1.0/n+sum(n-1);
+}
 
 double sum_series(int n){
+    if (n<=0)
     return -1.0;
+    double x=sum(n);
+    return sum(n);
+
 }
