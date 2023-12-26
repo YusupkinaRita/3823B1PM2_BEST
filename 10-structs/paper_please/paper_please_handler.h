0a66212be3ccd48f9bbec8cb339ea9b8e977b104
YusupkinaRita <yusupkina.rita@gmail.com>
2023-12-26 21:24:37 +0300

    jkl

--- 10-structs/paper_please/paper_please_handler.h
+++ 10-structs/paper_please/paper_please_handler.h
@@ -26,5 +26,22 @@ struct Person
 
 
 unsigned char paper_please_game(struct Person* persons, unsigned char size){
-    return 0;
+    int pass_num=0;
+    
+    for(int i=0;i<size; i++){
+        int indicator=0;
+        if((2023-persons[i].pasport.bornDate.year)>40)
+            indicator++;
+        if(persons[i].pasport.country!="Arstocka"&&persons[i].pasport.country!="Arcanistan")
+            indicator++;
+        if(persons[i].working_specialty!="programmist"&&persons[i].working_specialty!="major"&&persons[i].working_specialty!="collective farmer")
+            indicator++;
+        if(persons[i].driver_license!=66)
+            indicator++;
+        if (indicator==0)
+            pass_num++;
+
+        
+    }
+    return pass_num;
 }
